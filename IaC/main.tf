2384fb7d05d40becc6d797b6f5d67e4762985735
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2023-12-27 10:30:18 +0000

    Update storage account and metastore names

--- IaC/main.tf
+++ IaC/main.tf
@@ -25,16 +25,16 @@ module "examples_adb-lakehouse" {
   key_vault_name                  = "TjxmUnityVault"
   location                        = "eastus"
   managed_resource_group_name     = "TjxmAnalyticsMgr-RG"
-  metastore_storage_name          = "Tjxmtastore"
+  metastore_storage_name          = "tjxmtastore"
   private_subnet_address_prefixes = ["10.0.3.0/24"]
   project_name                    = "TjxmAdvancedAnalytics"
   public_subnet_address_prefixes  = ["10.0.4.0/24"]
   shared_resource_group_name      = "TjxmAnalytics-RG"
   spoke_resource_group_name       = "TjxmAnalyticsSpoke-RG"
   spoke_vnet_address_space        = "10.0.0.0/16"
-  storage_account_names           = ["Tjxmastorage"]
+  storage_account_names           = ["tjxmastorage02"]
   tags = { 
-    environment = "Env01",
+    environment = "env01",
     MEN = "Data"
   }
 }
